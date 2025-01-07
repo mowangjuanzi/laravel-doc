f51874c644163d4df1f4c7f5ddcef6f45ad0dca8
mowangjuanzi <baoguoxiao0538@hotmail.com>
2025-01-07 09:16:53 +0800

    Update

---
+++ .vitepress/config.mjs
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Laravel 文档",
+  description: "Laravel 文档中文版",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      {
+        text: '11.x', link: '/11',
+      },
+    ],
+
+    sidebar:  {
+      "/11.x": [
+        {
+          text: "序言",
+          items: [
+            {
+              text: "发行说明",
+              link: "/11.x/releases.md"
+            },
+            {
+              text: "升级指南",
+              link: "/11.x/upgrade.md"
+            },
+            {
+              text: "贡献指南",
+              link: "/11.x/contributions.md"
+            }
+          ]
+        },
+        {
+          text: "新手入门",
+          items: [
+            {
+              text: "安装",
+              link: "/11.x/installation.md"
+            }
+          ]
+        }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,14 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Laravel 中文文档"
+  text: "面向 Web 工匠的 PHP 框架"
+  tagline: Laravel 是拥有表达力强且优雅语法的 web 应用框架。已经为您奠定了坚实的基础，使您能够专注于创造，而不必为琐事烦恼。
+  actions:
+    - theme: brand
+      text: 新手入门
+      link: /11.x/installation.md
+---
+
